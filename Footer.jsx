c7808d2277222f3137658e52f1ca22d52ad02364
Rooban Chakravarthi S <150768524+RoobanChakravarthiS@users.noreply.github.com>
2024-08-11 14:38:40 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-
-function Footer() {
+import { Navigate, useNavigate, } from 'react-router-dom'
+function Footer(props) {
+  const navigate = useNavigate();
   return (
     <>
         <section className='  h-16 background-lg bg-white'>
             <div className="bg-white h-full flex flex-row justify-between">
 
                 <div id='home'>
-                <button><span className="material-symbols-outlined text-5xl">home</span></button>
+                <button onClick={()=>navigate('/home')}><span className="material-symbols-outlined text-5xl">home</span></button>
                 </div>
+
                 <div id='favourites'>
-                <button><span className="material-symbols-outlined  text-5xl">favorite</span></button>
+
+                <button onClick={()=>navigate('/favourite')}><span className="material-symbols-outlined  text-5xl">favorite</span></button>
                 </div>
                 <div id='cart'>
                 <button><span className="material-symbols-outlined text-5xl">shopping_cart</span></button>
