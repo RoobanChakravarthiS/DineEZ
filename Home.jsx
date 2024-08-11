039568e5afae327543e673a1d81a833becf7e506
Rooban Chakravarthi S <150768524+RoobanChakravarthiS@users.noreply.github.com>
2024-08-11 15:24:26 +0530

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -3,17 +3,17 @@ import Header from './header'
 import Footer from './Footer'
 import Cards from './Cards'
 import Search from './Search'
-
+import { useState } from 'react'
 
 
 function Home(props) {
-  
+  const [query, setQuery] = useState('');
   return (
     <>
     <div className='h-dvh bg-[#fff7ed]'>
       <Header styles={props.styles} wishList={props.wishList} setWishList={props.setWishList} handleClick={props.handleClick}/>
-      <Search styles={props.styles} wishList={props.wishList} setWishList={props.setWishList} handleClick={props.handleClick}/>
-      <Cards styles={props.styles} wishList={props.wishList} setWishList={props.setWishList} handleClick={props.handleClick}/>
+      <Search query={query} setQuery={setQuery} styles={props.styles} wishList={props.wishList} setWishList={props.setWishList} handleClick={props.handleClick}/>
+      <Cards query={query} styles={props.styles} wishList={props.wishList} setWishList={props.setWishList} handleClick={props.handleClick}/>
       <Footer styles={props.styles} wishList={props.wishList} setWishList={props.setWishList} handleClick={props.handleClick}/>
     </div>
     </>
